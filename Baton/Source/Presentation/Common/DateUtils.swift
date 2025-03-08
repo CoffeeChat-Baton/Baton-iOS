9b0e72aaf8b37a5314594eb5510bdea7b3aa1f5e
Park-kxng <coding.parkgy29@gmail.com>
2025-03-08 18:17:32 +0900

    feat: dateUtils 추가

---
+++ Baton/Source/Presentation/Common/DateUtils.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+enum DateUtils {
+    static func formatToKoreanDate(_ dateString: String) -> String? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        formatter.locale = Locale(identifier: "ko_KR")
+        
+        guard let date = formatter.date(from: dateString) else { return nil }
+        
+        let resultFormatter = DateFormatter()
+        resultFormatter.dateFormat = "M월 d일 (E)" // ✅ "10월 31일 (금)" 형태
+        resultFormatter.locale = Locale(identifier: "ko_KR")
+        
+        return resultFormatter.string(from: date)
+    }
+}
